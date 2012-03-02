3eb1be0ebb9d53223a9b656d0f021c10abdaa623
Owen Pletz <voc.opletz@yahoo.com>
2012-03-02 11:45:47 -0800

    Cucumber version changed and ran cucumber generator

--- Gemfile
+++ Gemfile
@@ -34,7 +34,7 @@ group :test, :development do
 end
 
 group :test do
-  gem 'cucumber-rails'
+  gem 'cucumber-rails', '1.0.6'
   gem 'capybara'
   gem 'database_cleaner'
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -41,19 +41,10 @@ end
 # See the DatabaseCleaner documentation for details. Example:
 #
 #   Before('@no-txn,@selenium,@culerity,@celerity,@javascript') do
-#     # { :except => [:widgets] } may not do what you expect here
-#     # as tCucumber::Rails::Database.javascript_strategy overrides
-#     # this setting.
-#     DatabaseCleaner.strategy = :truncation
+#     DatabaseCleaner.strategy = :truncation, {:except => %w[widgets]}
 #   end
 #
 #   Before('~@no-txn', '~@selenium', '~@culerity', '~@celerity', '~@javascript') do
 #     DatabaseCleaner.strategy = :transaction
 #   end
 #
-
-# Possible values are :truncation and :transaction
-# The :transaction strategy is faster, but might give you threading problems.
-# See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
-Cucumber::Rails::Database.javascript_strategy = :truncation
-
